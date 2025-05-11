c09d0e73f4d5f34eef35398baa33c00f3136e3b6
shubhajit-paul-web <shubhajitpaul717@gmail.com>
2025-05-11 22:59:52 +0530

    fireworks animation added whenever todo's created

--- app.js
+++ app.js
@@ -135,6 +135,41 @@ function handleTodoList() {
 				displayData(); // Refresh the displayed data
 				editTodo(); // Enable edit functionality for the new todo
 				deleteTodo(); // Enable delete functionality for the new todo
+
+				// Fireworks animation
+				(function () {
+					const duration = 3 * 1000,
+						animationEnd = Date.now() + duration,
+						defaults = {startVelocity: 30, spread: 360, ticks: 60, zIndex: 9999};
+
+					function randomInRange(min, max) {
+						return Math.random() * (max - min) + min;
+					}
+
+					const interval = setInterval(function () {
+						const timeLeft = animationEnd - Date.now();
+
+						if (timeLeft <= 0) {
+							return clearInterval(interval);
+						}
+
+						const particleCount = 50 * (timeLeft / duration);
+
+						// since particles fall down, start a bit higher than random
+						confetti(
+							Object.assign({}, defaults, {
+								particleCount,
+								origin: {x: randomInRange(0.1, 0.3), y: Math.random() - 0.2},
+							})
+						);
+						confetti(
+							Object.assign({}, defaults, {
+								particleCount,
+								origin: {x: randomInRange(0.7, 0.9), y: Math.random() - 0.2},
+							})
+						);
+					}, 250);
+				})();
 			}
 
 			// Reset form input fields after adding a todo
